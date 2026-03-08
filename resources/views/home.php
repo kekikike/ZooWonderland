de97bc5ffabedb579f4e335f55a53f4e42c906eb
Bluederer <alejandrachavezpaye06@gmail.com>
2026-03-08 17:47:47 -0400

    logout funcional

--- resources/views/home.php
+++ resources/views/home.php
@@ -279,7 +279,7 @@ $recorridos = $recorridos ?? [];
                             <a href="index.php?r=historial" title="Mi Historial"><i class="fa-solid fa-clock-rotate-left"></i></a>
                         <?php endif; ?>
                         <a href="index.php?r=perfil" title="Mi Perfil"><i class="fa-solid fa-circle-user"></i></a>
-                        <a href="index.php?r=logout" class="logout" title="Cerrar Sesión"><i class="fa-solid fa-power-off"></i></a>
+                        <a href="/logout" class="logout" title="Cerrar Sesión"><i class="fa-solid fa-power-off"></i></a>
                     </div>
                 </div>
             <?php else: ?>
